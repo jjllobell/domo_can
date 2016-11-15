d2508ad7834ee48affdbe40379f69fa53e2c0f8d
jjllobell <eb5bon@gmail.com>
2016-11-15 11:00:35 +0100

    work whit lib serial software on usb gateway

--- arduino/usb_can_txrx_nano/usb_can_txrx_nano.ino
+++ arduino/usb_can_txrx_nano/usb_can_txrx_nano.ino
@@ -4,7 +4,7 @@
 
 #include <mcp_can.h>
 #include <SPI.h>
-#include <SerialSoftware>
+#include <SoftwareSerial.h>
 
 long unsigned int rxId;
 unsigned char len = 0;
@@ -13,24 +13,34 @@ char msgString[128];                        // Array to store serial string
 
 #define CAN0_INT 2                              // Set INT to pin 2
 MCP_CAN CAN0(10);                               // Set CS to pin 10
+SoftwareSerial usb_pc(4, 3);                // RX, TX
 
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(19200);
+  usb_pc.begin(19200);
+  CAN0.begin(MCP_ANY, CAN_250KBPS, MCP_8MHZ);
+  delay(1);
   
   // Initialize MCP2515 running at 8MHz with a baudrate of 250kb/s and the masks and filters disabled, 
   // special config. to MCP2515 - TJA1050 - NiRen Module
-  if(CAN0.begin(MCP_ANY, CAN_250KBPS, MCP_8MHZ) == CAN_OK)
-    Serial.println("MCP2515 Initialized Successfully!");
-  else
+ // if(CAN0.begin(MCP_ANY, CAN_250KBPS, MCP_8MHZ) == CAN_OK){
+ if (CAN_OK) {
+    Serial.println("MCP2515 - TJA1050 Initialized Successfully!");
+    usb_pc.println("MCP2515 - TJA1050 Initialized Successfully!");
+  }
+  else {
     Serial.println("Error Initializing MCP2515...");
+    usb_pc.println("Error Initializing MCP2515...");
+  }
   
   CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
 
   pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
   
-  Serial.println("MCP2515 - Ready to receive!!");
+  Serial.println("MCP2515-TJA1050 - Ready to receive!!");
+  usb_pc.println("MCP2515-TJA1050 - Ready to receive!!");
 
 }
 
@@ -43,11 +53,15 @@ void loop() {
         if(rxBuf[i] < 0x10)                // If data byte is less than 0x10, add a leading zero
         {
           Serial.print("0");
+          usb_pc.print("0");
         }
-        Serial.print(rxBuf[i], HEX);
+        Serial.print(rxBuf[i], HEX);        // Print data of index i
+        usb_pc.print(rxBuf[i], HEX);
         Serial.print(" ");
+        usb_pc.print(" ");
       }
       Serial.println();
+      usb_pc.println();
     }
 
 }
